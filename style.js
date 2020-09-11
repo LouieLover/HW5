6c6e0265cb6af21e5d8e556341d052a1ff737dfc
Larry X <papadocarock@gmail.com>
2020-09-11 14:14:30 -0400

    rows

--- style.js
+++ style.js
@@ -1,13 +1,32 @@
-var a = moment().format();
-
-$("#today").append(a);
-
-var timeStamp = moment().startOf("day").fromNow();
-console.log(timeStamp);
-
-var textArea = $("textarea");
-console.log(textArea)
-
-function hourStyle() {
-    for (i = 0; i < textArea.length; i++);
+var today = moment().format();
+var now = moment().format();
+var workDay = [
+    { time: "1 AM", event: "" },
+    { time: "2 AM", event: "" },
+    { time: "3 AM", event: "" },
+    { time: "4 AM", event: "" },
+    { time: "5 AM", event: "" },
+    { time: "6 AM", event: "" },
+    { time: "7 AM", event: "" },
+    { time: "8 AM", event: "" },
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 AM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+    { time: "6 PM", event: "" },
+    { time: "7 PM", event: "" },
+    { time: "8 PM", event: "" },
+    { time: "9 PM", event: "" },
+    { time: "10 PM", event: "" },
+    { time: "11 PM", event: "" },
+    { time: "12 PM", event: "" },
+];
+var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
+if (checkPrevious !== null) {
+    planWorkday = checkPrevious;
 }
