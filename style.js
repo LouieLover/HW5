9dc6e3266673cf7217ac42432f80d620cdf76860
Larry X <papadocarock@gmail.com>
2020-09-11 21:34:10 -0400

    hahah

--- style.js
+++ style.js
@@ -26,7 +26,33 @@ var workDay = [
     { time: "11 PM", event: "" },
     { time: "12 PM", event: "" },
 ];
-var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
-if (checkPrevious !== null) {
-    planWorkday = checkPrevious;
-}
+$(document).ready(function() {
+            let today = moment().format('dddd.MMMM Do, YYY h:mm a'
+
+                let now = moment().format(
+                    "1.AM"
+                    "2.AM"
+                    "3.AM"
+                    "4.AM"
+                    "5.AM"
+                    "6.AM"
+                    "7.AM"
+                    "8.AM"
+                    "9.AM"
+                    "10.AM"
+                    "11.AM"
+                    "12.AM"
+                    "1.PM"
+                    "2.PM"
+                    "3.PM"
+                    "4.PM"
+                    "5.PM"
+                    "6.PM"
+                    "7.PM"
+                    "8.PM"
+                    "9.PM"
+                    "10.PM"
+                    "11.PM"
+                    "12.PM")
+
+            });
