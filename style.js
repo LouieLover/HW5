3fee54e5fcefafb42aa277d502da2e854c50a8a0
Larry X <papadocarock@gmail.com>
2020-09-11 21:57:42 -0400

    columns

--- style.js
+++ style.js
@@ -27,32 +27,45 @@ var workDay = [
     { time: "12 PM", event: "" },
 ];
 $(document).ready(function() {
-            let today = moment().format('dddd.MMMM Do, YYY h:mm a'
+    console.log(workDay)
+    var today = moment().format('dddd.MMMM Do, YYY h:mm a');
 
-                let now = moment().format(
-                    "1.AM"
-                    "2.AM"
-                    "3.AM"
-                    "4.AM"
-                    "5.AM"
-                    "6.AM"
-                    "7.AM"
-                    "8.AM"
-                    "9.AM"
-                    "10.AM"
-                    "11.AM"
-                    "12.AM"
-                    "1.PM"
-                    "2.PM"
-                    "3.PM"
-                    "4.PM"
-                    "5.PM"
-                    "6.PM"
-                    "7.PM"
-                    "8.PM"
-                    "9.PM"
-                    "10.PM"
-                    "11.PM"
-                    "12.PM")
+    var now = moment().format()
+    "1.AM"
+    "2.AM"
+    "3.AM"
+    "4.AM"
+    "5.AM"
+    "6.AM"
+    "7.AM"
+    "8.AM"
+    "9.AM"
+    "10.AM"
+    "11.AM"
+    "12.AM"
+    "1.PM"
+    "2.PM"
+    "3.PM"
+    "4.PM"
+    "5.PM"
+    "6.PM"
+    "7.PM"
+    "8.PM"
+    "9.PM"
+    "10.PM"
+    "11.PM"
+    "12.PM"
 
-            });
+});
+
+let workDay = $("today").text(momentVar);
+return workDay;
+
+
+workDay();
+let row;
+let timeCol;
+
+for (i + 0; i < $("<div>").addClass("row"); {
+
+    }
